fbc3b4638777b92f2838e3b776d31345cf1c41b6
SachiP <sachi_p@pixiv.com>
2020-12-01 21:21:02 +0900

    お〜

--- test/pipeline/processable_pipeline.cpp
+++ test/pipeline/processable_pipeline.cpp
@@ -25,16 +25,10 @@ TEST(ProcessablePipelineConceptTest, LRTest) {
 }
 
 TEST(ProcessablePipelineConceptTest, RLTest) {
-    decltype(auto) connection = Connection<int, int>{};
-    decltype(auto) processable = 11 | connection;
-    std::cout << typeid(processable).name() << ":" << typeid(decltype(processable.process())).name() << ":" << processable.process() << std::endl;
-    std::cout << typeid(11 | connection).name() << ":" << typeid(decltype((11 | connection).process())).name() << ":" << (11 | connection).process() << std::endl;
+    decltype(auto) connection = Connection<bool, bool>{};
+    decltype(auto) processable = true | connection;
     ASSERT_TRUE(processable.process());
-    ASSERT_TRUE((11 | connection).process());
-    decltype(auto) connection0 = Connection<bool, bool>{};
-    decltype(auto) processable0 = true | connection;
-    ASSERT_TRUE(processable0.process());
-    ASSERT_TRUE((true | connection0).process());
+    ASSERT_TRUE((true | connection).process());
 }
 
 TEST(ProcessablePipelineConceptTest, RRTest) {
