beb8f4da5033d695ec76ec1ab6530eb2c49a565e
SachiSakurane <sakuranesachi@gmail.com>
2021-08-31 15:27:13 +0900

    size_t

--- include/owle/concepts/base_bus.hpp
+++ include/owle/concepts/base_bus.hpp
@@ -5,7 +5,7 @@
 namespace owle {
 template <class BaseBusType>
 concept base_bus = requires(const BaseBusType &b) {
-  { b.channel() } -> std::same_as<size_t>;
-  { b.size() } -> std::same_as<size_t>;
+  { b.channel() } -> std::same_as<std::size_t>;
+  { b.size() } -> std::same_as<std::size_t>;
 };
 } // namespace owle
--- include/owle/concepts/writable_bus.hpp
+++ include/owle/concepts/writable_bus.hpp
@@ -8,6 +8,6 @@ namespace owle {
 template <class WritableBusType>
 concept writable_bus = owle::base_bus<WritableBusType> && requires(WritableBusType &w) {
   typename WritableBusType::value_type;
-  { w.data(std::declval<size_t>()) } -> std::same_as<typename WritableBusType::value_type *>;
+  { w.data(std::declval<std::size_t>()) } -> std::same_as<typename WritableBusType::value_type *>;
 };
 } // namespace owle
