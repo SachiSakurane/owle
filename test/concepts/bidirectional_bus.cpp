cdd9ae53da766739a7f8b3499b06fd1b021eb24f
SachiSakurane <sakuranesachi@gmail.com>
2021-08-31 15:25:56 +0900

    fix test

--- test/concepts/bidirectional_bus.cpp
+++ test/concepts/bidirectional_bus.cpp
@@ -1,19 +1,14 @@
-#include <gtest/gtest.h>
 #include <owle/concepts/bidirectional_bus.hpp>
 
 #include "mock_buses.hpp"
 
-bool StaticBidirectionalBusTest() {
-  static_assert(!owle::BidirectionalBus<BasicBaseBus>,
-                "BasicBaseBus hasn't BidirectionalBus concept");
-  static_assert(!owle::BidirectionalBus<ImmutableReadableBus<int>>,
-                "ImmutableReadableBus<int> hasn't BidirectionalBus concept");
-  static_assert(!owle::BidirectionalBus<MutableWritableBus<int>>,
-                "MutableWritableBus<int> hasn't BidirectionalBus concept");
-  static_assert(owle::BidirectionalBus<BidirectionalBus<int>>,
-                "BidirectionalBus<int> has BidirectionalBus concept");
-
-  return true;
-}
-
-TEST(BidirectionalBusConceptTest, StaticTest) { ASSERT_TRUE(StaticBidirectionalBusTest()); }
+namespace {
+static_assert(!owle::BidirectionalBus<BasicBaseBus>,
+              "BasicBaseBus hasn't BidirectionalBus concept");
+static_assert(!owle::BidirectionalBus<ImmutableReadableBus<int>>,
+              "ImmutableReadableBus<int> hasn't BidirectionalBus concept");
+static_assert(!owle::BidirectionalBus<MutableWritableBus<int>>,
+              "MutableWritableBus<int> hasn't BidirectionalBus concept");
+static_assert(owle::BidirectionalBus<BidirectionalBus<int>>,
+              "BidirectionalBus<int> has BidirectionalBus concept");
+} // namespace
